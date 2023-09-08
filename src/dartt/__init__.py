922d6abdd3db97cdaf3a38193be0686b36baa599
David Greene <dag@obbligato.org>
2023-09-08 16:17:26 -0500

    Initialize project
    Add hatch build setup.

---
+++ src/dartt/__init__.py
@@ -0,0 +1,48 @@
+# SPDX-FileCopyrightText: 2023-present David A. Greene <dag@obbligato.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+# Copyright 2023 David A. Greene
+
+# This file is part of dartt
+
+# dartt is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License along
+# with dartt. If not, see <https://www.gnu.org/licenses/>.
+
+"""
+============================================
+dartt - Disc Archive, Rip, Transcode and Tag
+============================================
+
+|VersionImageLink|_
+
+|PythonVersionImageLink|_
+
+-------
+
+Installation
+============
+::
+
+   pip install dartt
+
+License
+=======
+`dartt` is distributed under the terms of the `GNU General Public License v3.0 or later`_.
+
+.. |VersionImageLink| image:: https://img.shields.io/pypi/v/dartt.svg
+.. _VersionImageLink: https://pypi.org/project/dartt
+.. |PythonVersionImageLink| image:: https://img.shields.io/pypi/pyversions/dartt.svg
+.. _PythonVersionImageLink: https://pypi.org/project/dartt
+.. _`GNU General Public License v3.0 or later`: https://spdx.org/licenses/GPL-3.0-or-later.html
+
+"""
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present David A. Greene <dag@obbligato.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
