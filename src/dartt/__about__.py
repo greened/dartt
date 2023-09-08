14d2545afa5046a45e6b40fe46dffa432b03791d
David Greene <dag@obbligato.org>
2023-09-08 16:17:26 -0500

    Initialize project
    Add hatch build setup.

---
+++ src/dartt/__about__.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2023-present David A. Greene <dag@obbligato.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+# Copyright 2023 David A. Greene
+
+# This file is part of dartt
+
+# dartt is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License along
+# with dartt. If not, see <https://www.gnu.org/licenses/>.
+
+__version__ = "0.0.1"
---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+# SPDX-FileCopyrightText: 2023-present David A. Greene <dag@obbligato.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import dartt
+from dartt.main import main
+
+def test_main_no_args():
+    main()
